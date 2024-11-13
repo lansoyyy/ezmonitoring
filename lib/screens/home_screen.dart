8e7b6a4749b3efdd44c8b1d734491d9abec6fbe7
Lans12345 <olanalans12345@gmail.com>
2024-11-13 13:35:44 +0800

    added dashbioard tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ezmonitoring/screens/auth/login_screen.dart';
+import 'package:ezmonitoring/screens/tabs/dashboard_tab.dart';
 
 import 'package:ezmonitoring/utils/colors.dart';
 import 'package:ezmonitoring/widgets/logout_widget.dart';
@@ -16,107 +17,83 @@ class _HomeScreenState extends State<HomeScreen> {
   int index = 0;
 
   List items = [
-    'Number of Personnel',
-    'Vehicles',
-    'Logged Entry/Exit',
-    'Monitor\nRealtime Status',
-    'Generate Reports'
+    'Dashboard',
+    'Monitoring',
+    'Employee',
+    'Visitors',
+    'Report Logs'
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primary,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 75),
-          child: Row(
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                height: 35,
-              ),
-              const Expanded(
-                child: SizedBox(
-                  width: 20,
-                ),
-              ),
-              TextWidget(
-                text: 'Admin',
-                fontSize: 14,
-                fontFamily: 'Bold',
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              const Icon(
-                Icons.account_circle,
-                color: Colors.white,
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
-        child: Card(
-          color: Colors.black26,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30),
-                child: SizedBox(
-                  width: 400,
-                  height: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      for (int i = 0; i < items.length; i++)
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 5, top: 5),
-                          child: Card(
-                            color: Colors.amber,
-                            child: SizedBox(
-                              width: 200,
-                              child: ListTile(
-                                onTap: () {
-                                  setState(() {
-                                    index = i;
-                                  });
-                                },
-                                title: TextWidget(
-                                  text: items[i],
-                                  fontSize: 18,
-                                  fontFamily: 'Bold',
-                                  color:
-                                      index == i ? Colors.white : Colors.black,
-                                ),
-                              ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: SizedBox(
+              width: 200,
+              height: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: TextWidget(
+                      text: 'EZ Monitoring',
+                      fontSize: 24,
+                      fontFamily: 'Bold',
+                      color: Colors.white,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 150,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  for (int i = 0; i < items.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 5, top: 5),
+                      child: Card(
+                        color: Colors.white,
+                        child: SizedBox(
+                          width: 200,
+                          child: ListTile(
+                            onTap: () {
+                              setState(() {
+                                index = i;
+                              });
+                            },
+                            title: TextWidget(
+                              text: items[i],
+                              fontSize: 18,
+                              fontFamily: 'Bold',
+                              color: index == i ? Colors.blue : Colors.black,
                             ),
                           ),
                         ),
-                    ],
-                  ),
-                ),
+                      ),
+                    ),
+                ],
               ),
-              Expanded(
-                  child: Container(
-                color: Colors.grey[100],
-                child: IndexedStack(
-                  index: index,
-                  children: const [],
-                ),
-              )),
-            ],
+            ),
           ),
-        ),
+          Expanded(
+              child: Container(
+            color: Colors.grey[100],
+            child: IndexedStack(
+              index: index,
+              children: const [
+                DashboardTab(),
+              ],
+            ),
+          )),
+        ],
       ),
     );
   }
---
+++ lib/screens/tabs/dashboard_tab.dart
@@ -0,0 +1,187 @@
+import 'package:ezmonitoring/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+class DashboardTab extends StatelessWidget {
+  const DashboardTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 400,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[400],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.groups_2_outlined,
+                        size: 100,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextWidget(
+                        text: 'Total Employees',
+                        fontSize: 32,
+                        fontFamily: 'Bold',
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextWidget(
+                        text: '75',
+                        fontSize: 65,
+                        fontFamily: 'Bold',
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 400,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[400],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.groups_2_outlined,
+                        size: 100,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextWidget(
+                        text: 'Total Visitors',
+                        fontSize: 32,
+                        fontFamily: 'Bold',
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextWidget(
+                        text: '75',
+                        fontSize: 65,
+                        fontFamily: 'Bold',
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Wrap(
+              children: [
+                for (int i = 0; i < 5; i++)
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      width: 400,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.house_outlined,
+                            size: 100,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextWidget(
+                            text: 'Sector ${i + 1}',
+                            fontSize: 32,
+                            fontFamily: 'Bold',
+                            color: Colors.white,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextWidget(
+                                    text: 'Employees',
+                                    fontSize: 26,
+                                    fontFamily: 'Bold',
+                                    color: Colors.white,
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  TextWidget(
+                                    text: '75',
+                                    fontSize: 32,
+                                    fontFamily: 'Bold',
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextWidget(
+                                    text: 'Visitors',
+                                    fontSize: 26,
+                                    fontFamily: 'Bold',
+                                    color: Colors.white,
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  TextWidget(
+                                    text: '75',
+                                    fontSize: 32,
+                                    fontFamily: 'Bold',
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
